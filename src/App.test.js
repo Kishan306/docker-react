6ddb22a85ed1103ecee821e409b80a8897b3a0a4
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2025-01-06 18:04:47 +0530

    test change in app.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/kkk/i);
+  const linkElement = screen.getByText(/i was changed/i);
   expect(linkElement).toBeInTheDocument();
 });
